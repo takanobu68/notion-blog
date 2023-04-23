afb0f756342a8ace738532289e7ca01e8b16cd13
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-23 09:42:51 +0900

    詳細ページをAPIから取得したデータに変更

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -23,14 +23,18 @@ export const getStaticProps = async ({ params }) => {
 const Post = ({ post }) => {
   return (
     <section className='container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20'>
-      <h2 className='w-full text-2xl font-medium'>投稿</h2>
+      <h2 className='w-full text-2xl font-medium'>{post.metaData.title}</h2>
       <div className='border-b-2 w-1/3 mt-1 border-sky-900'></div>
-      <span className='text-gray-500 '>2012</span>
+      <span className='text-gray-500 '>
+        Posted date at {post.metaData.date}
+      </span>
       <br />
-      <p className='text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block'>
-        next.js
-      </p>
-      <div className='mt-10 font-medium'></div>
+      {post.metaData.tags.map((tag) => (
+        <p className='text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block mr-2'>
+          {tag}
+        </p>
+      ))}
+      <div className='mt-10 font-medium'>{post.markdown}</div>
     </section>
   );
 };
