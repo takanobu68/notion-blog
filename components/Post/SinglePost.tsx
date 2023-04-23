da2994b6280e5c09649a6e1084fe82ffeadb103c
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-23 12:20:42 +0900

    ページネーション専用ページのレイアウト作成

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -6,25 +6,49 @@ type Props = {
   date: string;
   tags: string[];
   slug: string;
+  isPaginationPage: boolean;
 };
 
 const SinglePost = (props: Props) => {
-  const { title, description, date, tags, slug } = props;
+  const { title, description, date, tags, slug, isPaginationPage } = props;
   return (
-    <section className='lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300'>
-      <div className='flex item-center gap-3'>
-        <Link href={`/posts/${slug}`}>
-          <h2 className='text-gray-100 text-2xl font-medium mb-2'>{title}</h2>
-        </Link>
-        <div className='text-gray-400 mr-2'>{date}</div>
-        {tags.map((tag) => (
-          <span className='text-white bg-gray-500 rounded-xl px-2 font-medium pb-1'>
-            {tag}
-          </span>
-        ))}
-      </div>
-      <p className='text-gray-100'>{description}</p>
-    </section>
+    <>
+      {isPaginationPage ? (
+        <section className='bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300'>
+          <div className='lg:flex item-center'>
+            <Link href={`/posts/${slug}`}>
+              <h2 className='text-gray-100 text-2xl font-medium mb-2'>
+                {title}
+              </h2>
+            </Link>
+            <div className='text-gray-400 mr-2'>{date}</div>
+            {tags.map((tag) => (
+              <span className='text-white bg-gray-500 rounded-xl px-2 font-medium mr-2'>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <p className='text-gray-100'>{description}</p>
+        </section>
+      ) : (
+        <section className='lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300'>
+          <div className='flex item-center gap-3'>
+            <Link href={`/posts/${slug}`}>
+              <h2 className='text-gray-100 text-2xl font-medium mb-2'>
+                {title}
+              </h2>
+            </Link>
+            <div className='text-gray-400 mr-2'>{date}</div>
+            {tags.map((tag) => (
+              <span className='text-white bg-gray-500 rounded-xl px-2 font-medium pb-1'>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <p className='text-gray-100'>{description}</p>
+        </section>
+      )}
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home({ fourPosts }) {
               date={post.date}
               tags={post.tags}
               slug={post.slug}
+              isPaginationPage={false}
             />
           </div>
         ))}
